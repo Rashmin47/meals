dd0d513313f231e380bfde013c1da696f952f3dc
rashmin <rashminxperience@gmail.com>
2026-01-13 16:20:20 +0545

    Added Meals Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:meals/data/dummy_data.dart';
 import 'package:meals/screens/categories.dart';
+import 'package:meals/screens/meals.dart';
 
 
 final theme = ThemeData(
@@ -23,7 +25,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
 
       theme: theme,
-      home: CategoriesScreen(),
+      home: MealsScreen(title: 'Some Category', meals: dummyMeals),
     );
   }
 }
